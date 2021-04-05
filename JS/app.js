6e0316a3819894f45569fafd9c0201380a16490e
mrkeyboard44 <matthewdandar@gmail.com>
2021-04-04 23:08:25 -0700

    added instructions in app.js

---
+++ JS/app.js
@@ -0,0 +1,6 @@
+// if statement for if the button is pressed the following function will run
+// create a function that saves the text
+// if statement for if the button is pressed the following function will run
+// create a funciton that loads a saved note
+// if statement for if the button is pressed the following function will run
+// create a function that removes the saved text and clears the page.
