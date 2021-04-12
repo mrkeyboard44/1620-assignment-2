94746d79627cd5aae4216e3a7e0d9060852d029b
mrkeyboard44 <matthewdandar@gmail.com>
2021-04-11 20:12:50 -0700

    added a textbox

--- JS/app.js
+++ JS/app.js
@@ -4,9 +4,28 @@
 // create a funciton that loads a saved note
 // if statement for if the button is pressed the following function will run
 // create a function that removes the saved text and clears the page.
-// const btn = document.querySelector('#dark_Theme');
+const notesArray = []
 
-// const current_theme = 'light';
+function defineNoteWritingSection() {
+    html = '\
+    <div id="note-area">\
+        <div id="note" contenteditable="true"></div>\
+        <button onclick="saveNote">save</button>\
+        <button onclick="cleanUp">cancel</button>\
+    </div>\
+    '
+    return html
+}
+
+function createContentWritingArea() {
+    const div = document.querySelector('#content')
+    div.insertAdjacentHTML('beforeend', defineNoteWritingSection())
+    console.log('writing area created')
+}
+
+const btn = document.querySelector('#dark_Theme');
+
+const current_theme = 'light';
 
 // function theme(element) {
 //     if (current_theme == 'light') {
@@ -33,10 +52,10 @@
 //     }
 //     return header_bcolor, body_bcolor, body_color
 // }
-// btn.onclick = function() {
-//     document.querySelector('header').style.backgroundColor = '#191724';
-//     document.body.style.backgroundColor = '#1f1d2e';
-//     document.querySelector('footer').style.backgroundColor = '#191724';
-//     document.body.style.color = '#e0def4';
-//     document.querySelectorAll('button > a').style.color = '#9ccfd8';
-// }
+btn.onclick = function() {
+    document.querySelector('header').style.backgroundColor = '#191724';
+    document.body.style.backgroundColor = '#1f1d2e';
+    document.querySelector('footer').style.backgroundColor = '#191724';
+    document.body.style.color = '#e0def4';
+    document.querySelectorAll('button > a').style.color = '#9ccfd8';
+}
