9497e1099e968225b204ae7fea7193f0e71f70b5
mrkeyboard44 <matthewdandar@gmail.com>
2021-04-11 19:39:58 -0700

    added plus and dark theme button, not usable yet.

--- JS/app.js
+++ JS/app.js
@@ -4,3 +4,39 @@
 // create a funciton that loads a saved note
 // if statement for if the button is pressed the following function will run
 // create a function that removes the saved text and clears the page.
+// const btn = document.querySelector('#dark_Theme');
+
+// const current_theme = 'light';
+
+// function theme(element) {
+//     if (current_theme == 'light') {
+//         const header_bcolor, footer_bcolor = '#191724';
+//         const body_bcolor = '#1f1d2e';
+//         const body_color = '#e0def4';
+//     }
+//     else {
+//         const header_bcolor, footer_bcolor = '#faf4ed';
+//         const body_bcolor = '#fffaf3';
+//         const body_color = '#575279';
+//     }
+//     if (element == 'header_bcolor') {
+//         return header_bcolor
+//     }
+//     if (element == 'footer_bcolor') {
+//         return footer_bcolor
+//     }
+//     if (element == 'body_bcolor') {
+//         return body_bcolor
+//     }
+//     if (element == 'body_color') {
+//         return body_color
+//     }
+//     return header_bcolor, body_bcolor, body_color
+// }
+// btn.onclick = function() {
+//     document.querySelector('header').style.backgroundColor = '#191724';
+//     document.body.style.backgroundColor = '#1f1d2e';
+//     document.querySelector('footer').style.backgroundColor = '#191724';
+//     document.body.style.color = '#e0def4';
+//     document.querySelectorAll('button > a').style.color = '#9ccfd8';
+// }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # 1620-assignment-2
-ACIT 1620 - Assignment 2 - By Matthew Dandar
+ACIT 1620 - Assignment 2 - By Matthew Dandar A00180450
