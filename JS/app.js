6b4062018fda4c0a25c51fe6c4ffe4bfe43f9303
mrkeyboard44 <matthewdandar@gmail.com>
2021-04-12 15:16:29 -0700

    added working dark theme button

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,3 @@
-// if statement for if the button is pressed the following function will run
-// create a function that saves the text
-// if statement for if the button is pressed the following function will run
-// create a funciton that loads a saved note
-// if statement for if the button is pressed the following function will run
-// create a function that removes the saved text and clears the page.
 const notesArray = []
 
 function defineNoteWritingSection() {
@@ -68,8 +62,6 @@ function createNoteObject(arr) {
     return newObject = { title: arr[0], body: arr[1] }
 }
 
-
-
 function defineSavedNotesSection() {
     html = '\
         <ul class="button" id="column">\
@@ -81,83 +73,102 @@ function defineSavedNotesSection() {
     return html
 }
 
-function defineCloseAsideButton() {
-    html = '\
-        <span onclick="closeSavedNotesSection()" class="button sidebar" id="close"><i class="fas fa-bars"></i></span>\
-        '
-    return html
+var asideStatus = "closed"
+
+function toggleSavedNotesSection() {
+    if (asideStatus == 'closed') {
+        openSavedNotesSection()
+    }
+    else {
+        closeSavedNotesSection()
+    }
 }
 
 function openSavedNotesSection() {
-    const div = document.querySelector('#view-note')
-    div.insertAdjacentHTML('beforeend', defineSavedNotesSection())
-    document.querySelector('aside').style.backgroundColor = '#f2e9de'
-    document.querySelector('#view-note').style.backgroundColor = '#f2e9de'
-    console.log('aside open')
-    const openAsideButton = document.querySelector("#open")
-    openAsideButton.remove()
-    const aside = document.querySelector('aside')
-    aside.insertAdjacentHTML('beforeend', defineCloseAsideButton())
-}
-
-function defineOpenAsideButton() {
-    html = '\
-        <span onclick="openSavedNotesSection()" class="button sidebar" id"open"><i class="fas fa-bars"></i></span>\
-        '
-    return html
+    const div = document.querySelector('#view-note');
+    div.insertAdjacentHTML('beforeend', defineSavedNotesSection());
+    document.querySelector('aside').style.backgroundColor = aside_bcolor;
+    document.querySelector('#view-note').style.backgroundColor = aside_bcolor;
+    console.log('aside open');
+    asideStatus = "open";
 }
 
 function closeSavedNotesSection() {
     const div = document.querySelector('#view-note')
     div.remove()
-    document.querySelector('aside').style.backgroundColor = '#fffaf3'
-    document.querySelector('#view-note').style.backgroundColor = '#fffaf3'
-    const button = document.querySelector("#close")
-    button.remove()
-    const aside = document.querySelector('aside')
-    aside.insertAdjacentHTML('beforeend', defineOpenAsideButton())
+    document.querySelector('aside').style.backgroundColor = body_bcolor
+    // document.querySelector('#view-note').style.backgroundColor = '#fffaf3'
+    asideStatus = 'closed'
 }
 
-function addNoteToNotesSection() {
+var themeColor = 'light'
 
+var header_bcolor = '#faf4ed'
+
+var body_bcolor = '#1f1d2e'
+
+var footer_bcolor = '#191724'
+
+var aside_bcolor = '#faf4ed'
+
+var text_color = '#e0def4'
+
+function defineDarkModeButton() {
+    html = '\
+    <button onclick="toggleTheme()" class="button" id="dark_mode"><a>Dark Theme</a></button>\
+    '
+    return html
 }
 
-const btn = document.querySelector('#dark_Theme');
+function defineLightModeButton() {
+    html = '\
+    <button onclick="toggleTheme()" class="button" id="light_mode"><a>Light Theme</a></button>\
+    '
+    return html
+}
 
-const themeColor = ''
+function toggleTheme() {
+    const theme = document.getElementById("dark_mode")
+    console.log(theme.firstChild.nodeValue)
+    if (theme.firstChild.nodeValue == 'Dark Theme') {
+        darkTheme(theme)
+    }
+    else {
+        lightTheme(theme)
+    }
+}
 
-// function theme(element) {
-//     if (current_theme == 'light') {
-//         const header_bcolor, footer_bcolor = '#191724';
-//         const body_bcolor = '#1f1d2e';
-//         const body_color = '#e0def4';
-//     }
-//     else {
-//         const header_bcolor, footer_bcolor = '#faf4ed';
-//         const body_bcolor = '#fffaf3';
-//         const body_color = '#575279';
-//     }
-//     if (element == 'header_bcolor') {
-//         return header_bcolor
-//     }
-//     if (element == 'footer_bcolor') {
-//         return footer_bcolor
-//     }
-//     if (element == 'body_bcolor') {
-//         return body_bcolor
-//     }
-//     if (element == 'body_color') {
-//         return body_color
-//     }
-//     return header_bcolor, body_bcolor, body_color
-// }
-btn.onclick = function() {
-    document.querySelector('header').style.backgroundColor = '#191724';
-    document.body.style.backgroundColor = '#1f1d2e';
-    document.querySelector('footer').style.backgroundColor = '#191724';
-    document.querySelector('aside').style.backgroundColor = '#26233a';
-    document.body.style.color = '#e0def4';
+function darkTheme(theme) {
+    header_bcolor = '#191724'
+    body_bcolor = '#1f1d2e'
+    footer_bcolor = '#191724'
+    aside_bcolor = '#26233a'
+    text_color = '#e0def4'
+    document.querySelector('header').style.backgroundColor = header_bcolor;
+    document.body.style.backgroundColor = body_bcolor;
+    document.querySelector('footer').style.backgroundColor = footer_bcolor;
+    if (asideStatus == 'closed') {
+        document.querySelector('aside').style.backgroundColor = body_bcolor;
+    }
+    else {
+        document.querySelector('aside').style.backgroundColor = aside_bcolor;
+    }
+    document.body.style.color = text_color;
     // document.querySelectorAll('button > a').style.color = '#9ccfd8';
-    const themeColor = 'dark'
+    theme.firstChild.nodeValue = 'Light Theme'
 }
-console.log(themeColor)
+
+function lightTheme(theme) {
+    header_bcolor = '#faf4ed'
+    body_bcolor = '#faf4ed'
+    footer_bcolor = '#faf4ed'
+    aside_bcolor = '#fffaf3'
+    text_color = '#575279'
+    document.querySelector('header').style.backgroundColor = '#faf4ed';
+    document.body.style.backgroundColor = '#faf4ed';
+    document.querySelector('footer').style.backgroundColor = '#faf4ed';
+    document.querySelector('aside').style.backgroundColor = '#fffaf3';
+    document.body.style.color = '#575279';
+    // document.querySelectorAll('button > a').style.color = '#9ccfd8';
+    theme.firstChild.nodeValue = 'Dark Theme'
+}
